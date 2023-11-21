906612f162b5b1fc155453eb9f2155fd9cd2c0c2
Jon Deaves <hi@jondeaves.me>
2023-11-21 16:11:41 -0500

    chore: team logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,49 @@
-import SimpleRiveComponent from "./RiveComponent"
+import { Button, Card, CardActions, CardContent } from "@mui/material"
 
-export const App = () => (
-  <main>
-    <SimpleRiveComponent />
-  </main>
-)
+import SimpleRiveComponent from "./RiveComponent"
+import { useState } from "react";
+
+export type Team = 'DOLPHINS' | 'TEXANS' | 'FALCONS'
+
+export const App = () => {
+  const [team, setTeam] = useState<Team|null>(null);
+
+  const onTogglePlay = (activeTeam: Team) => {
+    if (team) {
+      setTeam(null);
+    }
+
+    // Bit of a hack to ensure the image asset changes
+    setTimeout(() => {
+      setTeam(activeTeam);
+    }, 10);
+  }
+
+  return (
+    <main>
+      <Card sx={{ width: 375 }}>
+        <CardContent sx={{ padding: 0, height: 120 }}>
+          {team && (
+            <SimpleRiveComponent team={team} />
+          )}
+        </CardContent>
+
+        <CardActions sx={{ padding: '16px', justifyContent: 'space-between' }}>
+          <Button variant="contained" onClick={() => onTogglePlay('DOLPHINS')}>
+            Dolphins
+          </Button>
+
+          <Button variant="contained" onClick={() => onTogglePlay('TEXANS')}>
+            Texans
+          </Button>
+
+          <Button variant="contained" onClick={() => onTogglePlay('FALCONS')}>
+            Falcons
+          </Button>
+        </CardActions>
+      </Card>
+    </main>
+  );
+}
 
 export default App
--- src/RiveComponent.tsx
+++ src/RiveComponent.tsx
@@ -1,13 +1,9 @@
 import { useRive, Layout, Fit, Alignment, FileAsset, decodeImage } from '@rive-app/react-canvas';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
 
-import Button from '@mui/material/Button';
+import { Team } from './App';
+import { useEffect } from 'react';
 
 const RIVE_TEXT_RUN_LABEL = 'teamRun';
-
-type Team = 'DOLPHINS' | 'TEXANS' | 'FALCONS'
 
 const TEAM_MAPPING = {
   'DOLPHINS': 'MIAMI DOLPHINS',
@@ -15,7 +11,11 @@ const TEAM_MAPPING = {
   'FALCONS': 'ATLANTA FALCONS',
 }
 
-export const SimpleRiveComponent = () => {
+interface SimpleRiveComponentProps {
+  team: Team;
+}
+
+export const SimpleRiveComponent = ({ team }: SimpleRiveComponentProps) => {
   const { rive, RiveComponent } = useRive({
     src: '/new_file.riv',
     autoplay: false,
@@ -23,22 +23,9 @@ export const SimpleRiveComponent = () => {
       fit: Fit.FitWidth,
       alignment: Alignment.Center
     }),
-    assetLoader:(asset: FileAsset, bytes: Uint8Array) => {
-      console.log(
-        "Tell our asset importer if we are going to load the asset contents",
-        {
-          name: asset.name,
-          fileExtension: asset.fileExtension,
-          cdnUuid: asset.cdnUuid,
-          isFont: asset.isFont,
-          isImage: asset.isImage,
-          bytes
-        }
-      );
-
+    assetLoader: (asset: FileAsset) => {
       if (asset.name === 'teamLogo.png') {
-        fetch("/dolphins.png").then(async (res) => {
-          console.log("doing this");
+        fetch(`/${team.toLowerCase()}.png`).then(async (res) => {
           const image = await decodeImage(new Uint8Array(await res.arrayBuffer()));
 
           // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -55,49 +42,23 @@ export const SimpleRiveComponent = () => {
 
       return false;
     },
-    onLoad: () => {
-      if (!rive) {
-        return;
-      }
-
-      // Prevent a blurry canvas by using the device pixel ratio
-      rive.resizeDrawingSurfaceToCanvas();
-    }
   });
 
-  const onTogglePlay = (team: Team) => {
+  useEffect(() => {
     if (!rive) {
       return;
     }
 
-    const teamLabel = TEAM_MAPPING[team];
-    // const asset = `/${team}.png`;
+    // Prevent a blurry canvas by using the device pixel ratio
+    rive.resizeDrawingSurfaceToCanvas();
 
     rive.reset();
-    rive.setTextRunValue(RIVE_TEXT_RUN_LABEL, teamLabel)
+    rive.setTextRunValue(RIVE_TEXT_RUN_LABEL, TEAM_MAPPING[team]);
     rive.play();
-  }
+  });
 
   return (
-    <Card sx={{ width: 375 }}>
-      <CardContent sx={{ padding: 0, height: 120 }}>
-        <RiveComponent />
-      </CardContent>
-
-      <CardActions sx={{ padding: '16px', justifyContent: 'space-between' }}>
-        <Button variant="contained" disabled={!rive} onClick={() => onTogglePlay('DOLPHINS')}>
-          Dolphins
-        </Button>
-
-        <Button variant="contained" disabled={!rive} onClick={() => onTogglePlay('TEXANS')}>
-          Texans
-        </Button>
-
-        <Button variant="contained" disabled={!rive} onClick={() => onTogglePlay('FALCONS')}>
-          Falcons
-        </Button>
-      </CardActions>
-    </Card>
+    <RiveComponent />
   );
 }
 
